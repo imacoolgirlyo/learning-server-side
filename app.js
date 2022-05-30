03cf6c48155c49ec2f637eecb6d64f9d321267bc
imacoolgirlyo <tjrud1438@naver.com>
2022-05-30 21:54:19 +0900

    feat: create Dockerfile and build images

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require("express");
-const app = express();
-const port = 8001;
+// const express = require("express");
+// const app = express();
+// const port = 8001;
 
-app.get("/", (req, res) => {
-  res.send("Helloo Diana!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Helloo Diana!");
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
+
+const ronin = require("ronin-server");
+const mocks = require("ronin-mocks");
+
+const server = ronin.server();
+
+server.use("/", mocks.server(server.Router(), false, true));
+server.start();
